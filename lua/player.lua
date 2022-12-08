e5eeb1c301fb7c4e243c7c605796aad7fbc15932
Aquib <aquibjavedt007@gmail.com>
2022-12-08 22:53:00 +0530

    adding player.lua

---
+++ lua/player.lua
@@ -0,0 +1,143 @@
+-- TODO: major refacor needed (use ipc instead)
+-- TODO: mouse support, implement queue, fix playlist bug, (maybe an optional discord presence)
+-- TODO: dont update buffer extmark if buffer not shown
+
+--- Basically call the toggle_player function to toggle the player window
+--- <CR> will prompt for song name or youtube link to play (playlist is kinda buggy)
+--- p: pause/play                q: quit               m: mute
+--- >: next                      <: prev
+
+local M = {buf=nil, win=nil, ns=vim.api.nvim_create_namespace("player"), content_id=nil, title_id=nil}
+local conf = { width=50, height=5 }
+local state = {playing=false, jobid=nil, title="", paused=false, timing="", percent=0, muted=false, loaded=false}
+local win_opts = { relative='editor', style='minimal', border='single', row=0, col=0, height=conf.height, width=conf.width, title='PLAYER', title_pos='center' }
+
+-- NOTE: for statusline components
+M.music_info = function() return state end
+
+local by3 = (" "):rep(math.floor(conf.width/4)-1)
+local refresh_screen = function()
+    local dur = math.floor((state.percent/100) * conf.width)
+    vim.api.nvim_buf_set_extmark(M.buf, M.ns, 0, 0, {
+        virt_text = {{state.title, "Function"}}, virt_text_pos='overlay',
+        id=M.title_id
+    })
+
+    local time1, time2 = unpack(vim.split(state.timing, ' / '))
+    vim.api.nvim_buf_set_extmark(M.buf, M.ns, 0, 0, {
+        virt_lines={
+            {
+                {time1, "Function"}, {(" "):rep(conf.width - 16)}, {time2, "Function"}
+            },
+            {
+                {("▁"):rep(dur), "Function"}, {"", "PlayerGreen"}, {("▁"):rep(conf.width-dur), "Comment"}
+            },
+            {{"", ""}},
+            {
+                {by3.."ﲑ", "Function"}, {by3..(not state.paused and "" or ""), "Function"}, {by3.."ﲒ", "Function"}
+            }
+        },
+        id=M.content_id
+    })
+end
+
+M.toggle_player = function()
+    -- for p,_ in pairs(package.loaded) do if p:match("^player") then package.loaded[p]=nil end end
+    vim.api.nvim_set_hl(0, 'PlayerGreen', {fg="#95c561",underline=true, bold=true})
+    if state.loaded then
+        vim.api.nvim_win_hide(M.win)
+        state.loaded = false
+        return
+    end
+
+    M.buf = vim.api.nvim_create_buf(false, true)
+    M.win = vim.api.nvim_open_win(M.buf, true, win_opts)
+    -- DEBUG: purposes
+    -- vim.api.nvim_set_current_line("https://www.youtube.com/watch?v=gDXhFrYuZ7I")
+    M.title_id = vim.api.nvim_buf_set_extmark(M.buf, M.ns, 0, 0, {
+        virt_text={{state.title, "Function"}}, virt_text_pos='overlay'
+    })
+    M.content_id = vim.api.nvim_buf_set_extmark(M.buf, M.ns, 0, 0, {
+        virt_lines={
+            {
+                {state.timing, "Function"}
+            },
+            {
+                {("▁"):rep(math.floor((state.percent/100) * conf.width)), "Function"}
+            },
+            {{"", ""}},
+            {
+                {by3.."ﲑ", "Function"}, {by3..(state.paused and "" or ""), "Function"}, {by3.."ﲒ", "Function"}
+            }
+        },
+    })
+
+    vim.keymap.set({'n', 'i'}, '<CR>', function()
+        if state.playing then
+            vim.fn.jobstop(state.jobid)
+            state.playing = false
+            state.title = ""
+        end
+
+        -- local query = vim.api.nvim_get_current_line():gsub(" ", "+")
+        vim.ui.input({width=40}, function(query)
+            if query == "" then
+                vim.notify("Query not provided!")
+                return
+            end
+
+            if not query:match([[https://(www.)\?youtube.com]]) then
+                vim.pretty_print("Query not matched")
+                query = "ytdl://ytsearch:"..query
+            end
+
+            local command = {"mpv", "--term-playing-msg='${media-title}'", "--no-video", query}
+
+            state.jobid = vim.fn.jobstart(command, {
+                pty=true,
+                on_stdout=function(_, data)
+                    if data then
+                        local time = data[1]:match([[%d%d:%d%d:%d%d / %d%d:%d%d:%d%d]])
+                        local percent = data[1]:match([[(%d%d?%%)]])
+                        if percent then state.percent=percent:sub(1, -2) end
+
+                        if not data[1]:match("A:") and not data[1]:match("AO:") then
+                            state.title = data[1]:match([['(.*)']]) or state.title
+                        end
+
+                        if time then
+                            if state.timing ~= time then
+                                state.timing = time
+                                refresh_screen()
+                            end
+                        end
+                    end
+                end,
+            })
+            state.playing = true
+        end)
+    end, {buffer=M.buf})
+
+    local map = function(bind, to, fn)
+        vim.keymap.set('n', bind, function()
+            vim.api.nvim_chan_send(state.jobid, to)
+            if fn then fn() end
+            refresh_screen()
+        end, {buffer=M.buf})
+    end
+
+    map('q', 'q', function()
+        state.title = 'Not Playing.'
+        state.percent = 0
+        refresh_screen()
+    end)
+    map('p', 'p', function() state.paused = not state.paused end)
+    map('m', 'm', function() state.muted = not state.muted end)
+    map('>', '>')
+    map('<', '<')
+    -- map('<Left>', '\\e[[D')
+    -- map('<Right>', "\\e[[C")
+    state.loaded = true
+end
+
+return M
