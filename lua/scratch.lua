eb80328939b6acdb841a510441efd50cec02fe63
Aquib <aquibjavedt007@gmail.com>
2023-01-11 16:57:10 +0530

    setup components moved fully inside toggle()

--- lua/scratch.lua
+++ lua/scratch.lua
@@ -1,36 +1,28 @@
 local M = {}
 local buf, win
-local flag = false
+local loaded = false
 
-local eval = function()
-    local lines = vim.api.nvim_buf_get_lines(buf, 0, -1, false)
-    loadstring(table.concat(lines, '\n'))()
-end
+local eval = function() loadstring(table.concat(vim.api.nvim_buf_get_lines(buf, 0, -1, false), '\n'))() end
 
 M.toggle = function()
-    if flag then
-        vim.api.nvim_win_close(win, true)
-    else
+    if not loaded or not vim.api.nvim_win_is_valid(win) then
+        if not buf or not vim.api.nvim_buf_is_valid(buf) then
+            buf = vim.api.nvim_create_buf(false, true)
+            vim.api.nvim_buf_set_option(buf, 'ft', 'lua')
+            vim.api.nvim_buf_set_option(buf, 'bufhidden', 'hide')
+            vim.keymap.set('n', '<leader>r', eval, {buffer=buf})
+        end
+
         win = vim.api.nvim_open_win(buf, true, {
             relative='editor', border='rounded', style='minimal',
             row=0, col=math.ceil(vim.o.columns/2),
             height=math.ceil(vim.o.lines-3), width=math.ceil(vim.o.columns/2)
         })
+    else
+        vim.api.nvim_win_hide(win)
     end
-    flag = not flag
-end
 
-M.setup = function(opts)
-    opts = opts or {}
-
-    buf = vim.api.nvim_create_buf(false, true)
-    vim.api.nvim_buf_set_option(buf, 'ft', 'lua')
-    vim.api.nvim_buf_set_option(buf, 'bufhidden', 'hide')
-
-    vim.keymap.set('n', '<leader>r', eval, {buffer=buf})
-
-    vim.api.nvim_create_user_command('Scratch', M.toggle, {})
-    -- vim.api.nvim_create_user_command('ScratchEval', eval, {})
+    loaded = not loaded
 end
 
 return M
