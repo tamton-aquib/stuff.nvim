8e9d892a9a896342c73597989579733b745754c9
Tamton Aquib <aquibjavedt007@gmail.com>
2024-04-03 11:37:49 +0530

    ref: regroup vim.cmd's

--- lua/xkcd.lua
+++ lua/xkcd.lua
@@ -17,9 +17,7 @@ end
 
 M.xkcd = function()
     math.randomseed(os.clock())
-
-    vim.cmd [[vsp | enew | set nonu nornu bt=nofile bh=wipe]]
-    vim.cmd("vert resize -20")
+    vim.cmd [[vsp | enew | set nonu nornu bt=nofile bh=wipe | vert resize -20 ]]
 
     local num = math.random(1, 2750)
     vim.fn.jobstart({"curl", "https://xkcd.com/"..num.."/info.0.json"}, {
